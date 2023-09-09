95ece0e776120f3761d705362518269804770001
onurcihangir <onurcihangir1912@gmail.com>
2023-09-09 18:07:31 +0300

    feat: error and loading components && error handling for api requests

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -16,10 +16,11 @@ import {
   changeActiveFilters,
   getFlightsAsync,
 } from "../redux/flights/flightsSlice";
+import Loading from "./Loading";
+import Error from "./Error";
 
 function Filter() {
   const dispatch = useDispatch();
-  // TODO: control for fetching airports
   // TODO: validation for every field
   const isLoading = useSelector((state) => state.airports.isLoading);
   const error = useSelector((state) => state.airports.error);
@@ -37,101 +38,115 @@ function Filter() {
     dispatch(getFlightsAsync({ sortParam, filters: activeFilters }));
   };
 
+  if (isLoading) {
+    return <Loading loading={isLoading} />;
+  }
+
   return (
-    <Paper
-      elevation={3}
-      sx={{
-        width: "150vh",
-      }}
-    >
-      {/* <Stack
-        sx={{ padding: 2 }}
-        spacing={2}
-        direction="row"
-        justifyContent="center"
-        alignItems="center"
-      > */}
-      <Grid container spacing={2} sx={{ padding: 2, alignItems: "center" }}>
-        <Grid item xs={2} sm={1.5} md={1.5}>
-          <FormControlLabel
-            sx={{
-              width: "30vh",
-            }}
-            control={
-              <Checkbox checked={oneWay} onChange={() => setOneWay(!oneWay)} />
-            }
-            label="One Way"
-          />
+    <div>
+      {error && (
+        <Error open={error} errorMessage={"Error when fetching airports"} />
+      )}
+      <Paper
+        elevation={3}
+        sx={{
+          width: "150vh",
+        }}
+      >
+        <Grid container spacing={2} sx={{ padding: 2, alignItems: "center" }}>
+          <Grid item xs={2} sm={1.5} md={1.5}>
+            <FormControlLabel
+              sx={{
+                width: "30vh",
+              }}
+              control={
+                <Checkbox
+                  checked={oneWay}
+                  onChange={() => {
+                    const checked = !oneWay;
+                    setOneWay(checked);
+                    if (checked) {
+                      dispatch(
+                        changeActiveFilters({
+                          arrival_date: null,
+                        })
+                      );
+                    }
+                  }}
+                />
+              }
+              label="One Way"
+            />
+          </Grid>
+          <Grid item xs={10} sm={5.25} md={5.25}>
+            <Autocomplete
+              sx={{ width: "100%" }}
+              options={airports}
+              getOptionLabel={(option) => `(${option.code}) ${option.name}`}
+              renderInput={(params) => (
+                <TextField {...params} label="Departing From" />
+              )}
+              onChange={(event, value) =>
+                dispatch(changeActiveFilters({ dept_from: value.code }))
+              }
+            />
+          </Grid>
+          <Grid item xs={12} sm={5.25} md={5.25}>
+            <Autocomplete
+              sx={{ width: "100%" }}
+              options={airports}
+              getOptionLabel={(option) => `(${option.code}) ${option.name}`}
+              renderInput={(params) => (
+                <TextField {...params} label="Arriving At" />
+              )}
+              onChange={(event, value) =>
+                dispatch(changeActiveFilters({ arr_to: value.code }))
+              }
+            />
+          </Grid>
+          <Grid item xs={6} sm={5} md={5}>
+            <DatePicker
+              sx={{ width: "100%" }}
+              label={"Departure Date"}
+              onChange={(value) => {
+                let dateString = null;
+                if (value) {
+                  dateString = new Date(value);
+                }
+                dispatch(
+                  changeActiveFilters({
+                    departure_date: moment(dateString).format("YYYY-MM-DD"),
+                  })
+                );
+              }}
+            />
+          </Grid>
+          <Grid item xs={6} sm={5} md={5}>
+            <DatePicker
+              sx={{ width: "100%" }}
+              disabled={oneWay}
+              label={"Return Date"}
+              onChange={(value) => {
+                let dateString = null;
+                if (value) {
+                  dateString = new Date(value);
+                }
+                dispatch(
+                  changeActiveFilters({
+                    arrival_date: moment(dateString).format("YYYY-MM-DD"),
+                  })
+                );
+              }}
+            />
+          </Grid>
+          <Grid item xs={12} sm={2} md={2}>
+            <Button variant="contained" onClick={() => onClick()}>
+              Search Flights
+            </Button>
+          </Grid>
         </Grid>
-        <Grid item xs={10} sm={5.25} md={5.25}>
-          <Autocomplete
-            sx={{ width: "100%" }}
-            options={airports}
-            getOptionLabel={(option) => `(${option.code}) ${option.name}`}
-            renderInput={(params) => (
-              <TextField {...params} label="Departing From" />
-            )}
-            onChange={(event, value) =>
-              dispatch(changeActiveFilters({ dept_from: value.code }))
-            }
-          />
-        </Grid>
-        <Grid item xs={12} sm={5.25} md={5.25}>
-          <Autocomplete
-            sx={{ width: "100%" }}
-            options={airports}
-            getOptionLabel={(option) => `(${option.code}) ${option.name}`}
-            renderInput={(params) => (
-              <TextField {...params} label="Arriving At" />
-            )}
-            onChange={(event, value) =>
-              dispatch(changeActiveFilters({ arr_to: value.code }))
-            }
-          />
-        </Grid>
-        {/* </Stack>
-      <Stack
-        sx={{ padding: 2 }}
-        spacing={2}
-        direction="row"
-        justifyContent="space-between"
-        alignItems="center"
-      > */}
-        <Grid item xs={6} sm={5} md={5}>
-          <DatePicker
-            sx={{ width: "100%" }}
-            label={"Departure Date"}
-            onChange={(value) =>
-              dispatch(
-                changeActiveFilters({
-                  departure_date: moment(value).toISOString(true),
-                })
-              )
-            }
-          />
-        </Grid>
-        <Grid item xs={6} sm={5} md={5}>
-          <DatePicker
-            sx={{ width: "100%" }}
-            disabled={oneWay}
-            label={"Return Date"}
-            onChange={(value) =>
-              dispatch(
-                changeActiveFilters({
-                  arrival_date: moment(value).toISOString(true),
-                })
-              )
-            }
-          />
-        </Grid>
-        <Grid item xs={12} sm={2} md={2}>
-          <Button variant="contained" onClick={() => onClick()}>
-            Search Flights
-          </Button>
-        </Grid>
-        {/* </Stack> */}
-      </Grid>
-    </Paper>
+      </Paper>
+    </div>
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { Box } from "@mui/material";
 import Filter from "../components/Filter";
 import Sort from "../components/Sort";
 import FlightCard from "../components/FlightCard";
-import { useSelector } from "react-redux";
-import { Box } from "@mui/material";
+import Error from "../components/Error";
 
 function Home() {
   const flights = useSelector((state) => state.flights.items);
+  const error = useSelector((state) => state.flights.error);
   //TODO: control for error response for flight request
   //TODO: control for empty flight response
   return (
@@ -21,6 +23,9 @@ function Home() {
         paddingTop: "100px",
       }}
     >
+      {error && (
+        <Error open={error} errorMessage={"Error when fetching flights"} />
+      )}
       <Filter />
       <Sort />
       {flights.map((flight) => (
